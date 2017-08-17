695a5946eab50d50f4d18a0c0fd1e0eb8a3582d2
Hoài Nam <hoainamak521996@gmail.com>
2017-08-17 20:52:57 +0700

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,26 @@ var express  = require('express'),
     expressValidator = require('express-validator');
 
 
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 /*Set EJS template Engine*/
 app.set('views','./views');
 app.set('view engine','ejs');
@@ -17,6 +37,7 @@ app.use(expressValidator());
 /*MySql connection*/
 var connection  = require('express-myconnection'),
     mysql = require('mysql');
+
 
 app.use(
 
