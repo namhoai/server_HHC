d4ead2a9fac1218bfd6626df48d5054c1856b283
Hoài Nam <hoainamak521996@gmail.com>
2017-08-17 20:53:17 +0700

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var express  = require('express'),
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
